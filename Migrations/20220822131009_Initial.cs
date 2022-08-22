22206e640bae9c74785f0c42018bb20535df1ca3
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2022-08-22 09:29:54 -0400

    Initial Commit

---
+++ Migrations/20220822131009_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EntityFrameworkCoreInterceptors.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+using var db = new CustomerContext();
+db.Database.Migrate();
+
+var entity = db.Customers.Add(new Customer
+{
+    Name = "Khalid",
+    PhoneNumber = "(555) 867 5309"
+});
+
+db.SaveChanges();
+db.ChangeTracker.Clear();
+
+var customer = db.Customers.Find(entity.Entity.Id)!;
+Console.WriteLine($"{customer.Id}: {customer.Name} @ {customer.PhoneNumber} [{customer.Retrieved}]");
+
+public interface IHasRetrieved
+{
+    DateTime Retrieved { get; set; }
+}
+
+public class Customer : IHasRetrieved
+{
+    public int Id { get; set; } = 0;
+    public string Name { get; set; } = "";
+    public string? PhoneNumber { get; set; }
+
+    [NotMapped] public DateTime Retrieved { get; set; }
+}
+
+public class SetRetrievedInterceptor : IMaterializationInterceptor
+{
+    public object InitializedInstance(MaterializationInterceptionData materializationData, object instance)
+    {
+        if (instance is IHasRetrieved hasRetrieved)
+        {
+            hasRetrieved.Retrieved = DateTime.UtcNow;
+        }
+
+        return instance;
+    }
+}
+
+public class CustomerContext : DbContext
+{
+    private static readonly SetRetrievedInterceptor SetRetrievedInterceptor
+        = new();
+
+    public DbSet<Customer> Customers => Set<Customer>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .AddInterceptors(SetRetrievedInterceptor)
+            .UseSqlite("Data Source = customers.db");
+}
